a19262a8d43982b58a6a11594d7b66e7caaa246d
Florencia Accastelli <f.accastelli@gmail.com>
2024-05-24 12:32:25 -0300

    calculator working

---
+++ AccastelliFlorenciaTPN1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.accastelliflorenciatpn1;
+
+/**
+ *
+ * @author flor
+ */
+public class AccastelliFlorenciaTPN1 {
+
+    public static void main(String[] args) {
+        TPN1 v = new TPN1();
+        v.setVisible ( true ) ;
+}
+}
+    
+
